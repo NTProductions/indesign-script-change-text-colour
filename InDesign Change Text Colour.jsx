5ac28fd914d7294eb21036c3449f034ded0b433e
Nate Lovell <Natelovell99@gmail.com>
2021-06-03 00:23:57 -0600

    Create InDesign Change Text Colour.jsx

---
+++ InDesign Change Text Colour.jsx
@@ -0,0 +1,13 @@
+﻿var thisDocument = app.activeDocument;
+var textFrames = thisDocument.textFrames;
+if(thisDocument.colors.item("My Swatch") == null) {
+thisDocument.colors.add({name:"My Swatch", model:ColorModel.process, colorValue:[100, 0, 0, 0]});
+}
+
+for(var i = 0; i < textFrames.length; i++) {
+    theseLines = textFrames[i].lines;
+    for(var w = 0; w < theseLines.length; w++) {
+                    theseLines[w].fillColor = thisDocument.colors.item("My Swatch");
+            }
+    }
+
